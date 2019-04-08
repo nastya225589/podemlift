59bc57221a8ec3680e97335a2b7e7f1397949d12
Анастасия Полякова <p.anastasia@molinos.ru>
2019-04-08 18:56:42 +0300

    mask tel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 window.jQuery = window.$ = require('jquery');
 
 require('slick-carousel/slick/slick.js');
+require('./jquery.maskedinput.js');
 
 import Page from './pages/Page';
 new Page;
--- resources/js/pages/Page.js
+++ resources/js/pages/Page.js
@@ -1,11 +1,13 @@
 import Menu from '../partials/Menu';
 import ExamplesSlider from '../partials/ExamplesSlider';
 import ClientsSlider from '../partials/ClientsSlider';
+import Form from '../partials/Form';
 
 export default class Page {
     constructor() {
         new Menu;
         new ExamplesSlider;
         new ClientsSlider;
+        new Form;
     }
 }
