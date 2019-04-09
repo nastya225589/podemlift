cb8186079fffe066d2664ff8a4c11c500e18c54c
Анастасия Полякова <p.anastasia@molinos.ru>
2019-04-09 11:31:09 +0300

    npm jquery.maskedinput

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 window.jQuery = window.$ = require('jquery');
 
 require('slick-carousel/slick/slick.js');
-require('./jquery.maskedinput.js');
+require('jquery.maskedinput/src/jquery.maskedinput');
 
 import Page from './pages/Page';
 new Page;
