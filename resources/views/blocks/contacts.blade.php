8edfdd8db983985b4a63116730ce12dca8a52066
Анастасия Полякова <p.anastasia@molinos.ru>
2019-04-23 18:31:48 +0300

    add city

--- resources/views/blocks/contacts.blade.php
+++ resources/views/blocks/contacts.blade.php
@@ -4,9 +4,32 @@
             <a href="tel:+78002007815" class="contacts__link">8 (800) 200-78-15</a>
             <span>бесплатный звонок по России</span>
         </li>
-        <li class="contacts__item">
+        <li class="contacts__item contacts__city">
             <a href="tel:+78124093533" class="contacts__link">+7 (812) 409-35-33</a>
             <span class="contacts__city contacts__city--arrow">Санкт-Петербург</span>
+            <ul class="contacts__city-list">
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Москва</a>
+                </li>
+                <li class=contacts__city-item">
+                    <a href="" class="contacts__city-link">Санкт-Петербург</a>
+                </li>
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Омск</a>
+                </li>
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Екатеринбург</a>
+                </li>
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Уфа</a>
+                </li>
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Иркутск</a>
+                </li>
+                <li class="contacts__city-item">
+                    <a href="" class="contacts__city-link">Минск</a>
+                </li>
+            </ul>
         </li>
         <li class="contacts__item">
             <a href="/" class="contacts__link contacts__link--line">Контакты</a>
