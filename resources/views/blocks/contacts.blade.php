fa2aa22dcdb9f4569705fbd2eb0d3367f8cc9fd6
Анастасия Полякова <p.anastasia@molinos.ru>
2019-07-09 11:39:47 +0300

    fix block contacts

--- resources/views/blocks/contacts.blade.php
+++ resources/views/blocks/contacts.blade.php
@@ -6,30 +6,32 @@
         </li>
         <li class="contacts__item contacts__city  contacts__item--city">
             <a href="tel:+78124093533" class="contacts__link">+7 (812) 409-35-33</a>
-            <span class="contacts__city contacts__city--arrow">Санкт-Петербург</span>
-            <ul class="contacts__city-list">
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Москва</a>
-                </li>
-                <li class=contacts__city-item">
-                    <a href="" class="contacts__city-link">Санкт-Петербург</a>
-                </li>
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Омск</a>
-                </li>
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Екатеринбург</a>
-                </li>
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Уфа</a>
-                </li>
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Иркутск</a>
-                </li>
-                <li class="contacts__city-item">
-                    <a href="" class="contacts__city-link">Минск</a>
-                </li>
-            </ul>
+            <span class="contacts__city contacts__city--arrow">
+                Санкт-Петербург
+                <ul class="contacts__city-list">
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Москва</a>
+                    </li>
+                    <li class=contacts__city-item">
+                        <a href="" class="contacts__city-link contacts__city-link--active">Санкт-Петербург</a>
+                    </li>
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Омск</a>
+                    </li>
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Екатеринбург</a>
+                    </li>
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Уфа</a>
+                    </li>
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Иркутск</a>
+                    </li>
+                    <li class="contacts__city-item">
+                        <a href="" class="contacts__city-link">Минск</a>
+                    </li>
+                </ul>
+            </span>
         </li>
         <li class="contacts__item contacts__item--mail">
             <a href="mailto:info@p-ob.ru" class="contacts__link">info@p-ob.ru</a>
