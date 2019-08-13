7e5144417d08969d245a2a8ce2cf05f8521a9bc8
Kirill Osadhij <k.osadchij@molinos.ru>
2019-08-13 16:04:41 +0300

    Added js-cookie npm package

--- resources/js/partials/SortingView.js
+++ resources/js/partials/SortingView.js
@@ -1,15 +1,14 @@
 export default class SortingView {
   constructor () {
-    let now = new Date(Date.now());
-    let expires = new Date(now.setMonth(now.getMonth() + 1));
-    let domain = location.hostname;
+    const Cookies = require('js-cookie');
+    const domain = location.hostname;
 
     $('.sorting-view__btn--line').on('click', function () {
       $('.products__wrap').toggleClass('products__sorting-view');
       $('.card').toggleClass('card__sorting-view');
       $('.sorting-view__btn--line').toggleClass('sorting-view__btn-view--active');
       $('.sorting-view__btn--grid').removeClass('sorting-view__btn-view--active');
-      document.cookie = 'shorting_view_type=line; path=/; expires=' + expires + '; domain=' + domain;
+      Cookies.set('shorting_view_type', 'line', { expires: 30, domain: domain });
     });
 
     $('.sorting-view__btn--grid').on('click', function () {
@@ -17,7 +16,7 @@ export default class SortingView {
       $('.card').removeClass('card__sorting-view');
       $('.sorting-view__btn--grid').toggleClass('sorting-view__btn-view--active');
       $('.sorting-view__btn--line').removeClass('sorting-view__btn-view--active');
-      document.cookie = 'shorting_view_type=grid; path=/; expires=' + expires + '; domain=' + domain;
+      Cookies.set('shorting_view_type', 'grid', { expires: 30, domain: domain });
     });
   }
 };
