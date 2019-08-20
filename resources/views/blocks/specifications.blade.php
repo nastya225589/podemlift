92ff61f104f7b49de7c054dcf10c3fb6cac61290
Анастасия Полякова <p.anastasia@molinos.ru>
2019-08-20 17:10:07 +0300

    HELPDESK-6164 fix specifications & warranty

--- resources/views/blocks/specifications.blade.php
+++ resources/views/blocks/specifications.blade.php
@@ -1,34 +1,39 @@
 <ul class="specifications__list">
-    <li class="specifications__item specifications__item--weight-kg">
-        Грузоподъемность
+    <li class="specifications__item">
+        <img class="specifications__img" src="/images/icon/weight-kg.svg" alt="">
+        <span class="specifications__name">Грузоподъемность</span>
         <div class="specifications__dots">
             <div class="specifications__dot"></div>
             <span>250 кг</span>
         </div>
     </li>
-    <li class="specifications__item specifications__item--measure">
-        Высота подъема
+    <li class="specifications__item">
+        <img class="specifications__img" src="/images/icon/measure.svg" alt="">
+        <span class="specifications__name">Высота подъема</span>
         <div class="specifications__dots">
             <div class="specifications__dot"></div>
             <span>5 м</span>
         </div>
     </li>
-    <li class="specifications__item specifications__item--stopwatch">
-        Скорость подъема-опускания груза
+    <li class="specifications__item">
+        <img class="specifications__img" src="/images/icon/stopwatch.svg" alt="">
+        <span class="specifications__name">Скорость подъема-опускания груза</span>
         <div class="specifications__dots">
             <div class="specifications__dot"></div>
             <span>0,13 м/сек</span>
         </div>
     </li>
-    <li class="specifications__item specifications__item--paint-palette">
-        Цвет
+    <li class="specifications__item">
+        <img class="specifications__img" src="/images/icon/paint-palette.svg" alt="">
+        <span class="specifications__name">Цвет</span>
         <div class="specifications__dots">
             <div class="specifications__dot"></div>
             <span>По договоренности</span>
         </div>
     </li>
-    <li class="specifications__item specifications__item--lightning">
-        Электропривод
+    <li class="specifications__item">
+        <img class="specifications__img" src="/images/icon/lightning.svg" alt="">
+        <span class="specifications__name">Электропривод</span>
         <div class="specifications__dots">
             <div class="specifications__dot"></div>
             <span>Болгарский</span>
