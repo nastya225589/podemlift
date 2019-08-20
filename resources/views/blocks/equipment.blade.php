804b55a488fe75b7d527f94d6a40c3050f596103
Анастасия Полякова <p.anastasia@molinos.ru>
2019-08-20 18:09:46 +0300

    HELPDESK-6164 fix equipment

--- resources/views/blocks/equipment.blade.php
+++ resources/views/blocks/equipment.blade.php
@@ -2,16 +2,20 @@
     <h3 class="equipment__title title-h3">Для обеспечения безопасности, устройство оснащено:</h3>
 
     <ul class="equipment__list">
-        <li class="equipment__item equipment__item--weight">
+        <li class="equipment__item">
+            <img class="equipment__img" src="/images/icon/weight.svg" alt="">
             <span>Ограничителем грузоподъемности</span>
         </li>
-        <li class="equipment__item equipment__item--palm">
+        <li class="equipment__item">
+            <img class="equipment__img" src="/images/icon/palm.svg" alt="">
             <span>Автоматическим тормозом</span>
         </li>
-        <li class="equipment__item equipment__item--on-oof">
+        <li class="equipment__item">
+            <img class="equipment__img" src="/images/icon/on-oof.svg" alt="">
             <span>Концевым выключателем</span>
         </li>
-        <li class="equipment__item equipment__item--fence">
+        <li class="equipment__item">
+            <img class="equipment__img" src="/images/icon/fence.svg" alt="">
             <span>Оградой на площадке</span>
         </li>
     </ul>
