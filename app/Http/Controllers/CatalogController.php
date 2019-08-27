6195eb4ada54e7b9a5d0edfaf98df92a6bd84c12
kosadchij <kosadchij@inbox.ru>
2019-08-28 02:20:31 +0300

    CatalogController refactored

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ProductCategory;
+use App\Models\Product;
 use App\Models\ProductProperty;
 use App\Models\Redirect;
 use Illuminate\Http\Request;
@@ -10,91 +11,161 @@ use UserConfig;
 
 class CatalogController2 extends Controller
 {
-    public $urlProperty = null;
-    public $urlPropertyValue = null;
+    public function index(Request $request)
+    {
+        $params = $request->all();
+        $redirectUrl = $this->getSinglePropertyRedirect($request->path(), $params);
+        
+        if ($redirectUrl)
+            return redirect($redirectUrl);
+        
+        if (count($params))
+            $products = $this->filterService->filter($params)->published()->paginate(UserConfig::getProductsPerPageCount());
+        else
+            $products = Product::published()->paginate(UserConfig::getProductsPerPageCount());
+
+        $filters = $this->filterService->getFilters();
+
+        return view('catalog.category', [
+            'page' => $this->resource,
+            'products' => $products,
+            'filters' => $filters
+        ]);
+    }
 
     public function category($url, Request $request)
     {
         $params = $request->all();
+        
         $fullUrl = $this->resource->url . $url;
-        $category = $this->getCategory($fullUrl);
+        $category = ProductCategory::where('url', $fullUrl)->published()->first();
 
-        if ($this->urlProperty && $this->urlPropertyValue) {
-            $products = $this->filterService->filterSingle($this->urlProperty->slug, $this->urlPropertyValue);
-            $resetFiltersUrl = $this->resource->url . $this->withoutParametrizedUrl($url);
+        if (!$category && ($redirect = Redirect::getRedirect($fullUrl)))
+            return redirect($redirect[0], $redirect[1]);
+
+        $property = explode('/', $url)[1];
+        if (!$category && ProductProperty::where('slug', $property)->first()) {
+            $value = explode('/', $url)[2];
+            return $this->indexSingleFilter($property, $value, $request);
         }
-        else if (count($params))
+
+        $redirectUrl = $this->getSinglePropertyRedirect($request->path(), $params);
+        if ($redirectUrl)
+            return redirect($redirectUrl);
+
+        if (!$category)
+            abort(404, 'Страница не найдена');
+        
+        $filters = $this->filterService->getFilters($category);
+
+        if (count($params))
             $products = $this->filterService->filter($params, $category);
         else
             $products = $category->products();
 
         return view('catalog.category', [
-            'page' => $this->resource,
+            'page' => $category,
             'products' => $products->published()->paginate(UserConfig::getProductsPerPageCount()),
-            'filters' => $this->filterService->getFilters(),
-            'singleProperty' => $this->urlProperty->slug ?? null,
-            'singleValue' => $this->urlPropertyValue ?? null,
-            'resetFiltersUrl' => $resetFiltersUrl ?? null
+            'filters' => $filters
         ]);
     }
 
-    public function getCategory($url)
+    public function indexSingleFilter($property, $value, Request $request)
     {
-        $withoutParametrizedUrl = $this->withoutParametrizedUrl($url);
-        $category = ProductCategory::where('url', $url)->published()->first();
-        if (!$category && $this->isParametrizedUrl($url)){
-            $category = ProductCategory::where('url', $withoutParametrizedUrl)->published()->first();
-            
-            if (!$category && count(explode('/', $withoutParametrizedUrl)) <= 2)
-                return null;
-        }
+        $params = $request->all();
 
-        if (!$category)
-            $this->tryRedirect($url);
+        if ($redirectUrl = $this->getSinglePropertyRedirect($this->resource->url, $params))
+            return redirect($redirectUrl);
+        
+        if ($redirectUrl = $this->getMultiPropertyRedirect($request->getRequestUri(), null, $params))
+            return redirect($redirectUrl);
 
-        if (!$category)
-            $this->tryRedirect($withoutParametrizedUrl);
+        $filters = $this->filterService->getFilters();
+        $products = $this->filterService->filterSingle($property, $value);
+        return view('catalog.category', [
+            'page' => $this->resource,
+            'products' => $products->published()->paginate(UserConfig::getProductsPerPageCount()),
+            'filters' => $filters,
+            'singleProperty' => $property,
+            'singleValue' => $value,
+            'resetFiltersUrl' => $this->resource->url
+        ]);
+    }
 
-        if (!$category && $this->isParametrizedUrl($url) && count(explode('/', $withoutParametrizedUrl)) <= 2)
-            return null;
-            
+    public function singleFilterCategory($url, $property, $value, Request $request)
+    {
+        $params = $request->all();
+
+        if ($redirectUrl = $this->getSinglePropertyRedirect($this->resource->url . $url, $params))
+            return redirect($redirectUrl);
+        
+        if ($redirectUrl = $this->getMultiPropertyRedirect($request->getRequestUri(), $url, $params))
+            return redirect($redirectUrl);
+
+        $fullUrl = $this->resource->url . $url;
+        $category = ProductCategory::where('url', $fullUrl)->published()->first();
+
+        if (!$category && ($redirect = Redirect::getRedirect($fullUrl)))
+            return redirect($redirect[0], $redirect[1]);
+
         if (!$category)
             abort(404, 'Страница не найдена');
-
-        return $category;
+        
+        $filters = $this->filterService->getFilters($category);
+        $products = $this->filterService->filterSingle($property, $value, $category);
+        return view('catalog.category', [
+            'page' => $category,
+            'products' => $products->published()->paginate(UserConfig::getProductsPerPageCount()),
+            'filters' => $filters,
+            'singleProperty' => $property,
+            'singleValue' => $value,
+            'resetFiltersUrl' => $fullUrl
+        ]);
     }
 
-    public function tryRedirect($url)
+    public function product($slug)
     {
-        if ($redirect = Redirect::getRedirect($url)) {
-            redirect($redirect[0], $redirect[1]);
-            die();
+        $product = Product::where('slug', $slug)->published()->firstOrFail();
+        return view('catalog.product', [
+            'product' => $product
+        ]);
+    }
+
+    protected function getSinglePropertyRedirect($url, array $params)
+    {
+        if ($this->isSinglePropertyQuery($params)) {
+            if (is_countable($params[array_key_first($params)]))
+                return $url . '/' . array_key_first($params) . '/' . $params[array_key_first($params)][0];
+            else
+                return $url . '/' . array_key_first($params) . '/' . $params[array_key_first($params)];
         }
     }
 
-    public function isParametrizedUrl($url)
+    protected function getMultiPropertyRedirect($requestUri, $url, array $params)
     {
-        $url = explode('/', trim($url, '/'));
-        // /catalog/cvet/belyi
-        if (count($url) <= 2)
-            return false;
-
-        $paramValueSlug = array_pop($url);
-        $paramSlug = array_pop($url);
-
-        $this->urlProperty = ProductProperty::where('slug', $paramSlug)->first();
-        if (!$this->urlProperty)
-            return false;
-
-        $this->urlPropertyValue = $paramValueSlug;
-
-        return true;
+        if ($this->isMultyPropertyQuery($params)) {
+            $queryString = explode('?', $requestUri)[1];
+            return $this->resource->url . $url . '?' . $queryString;
+        }
     }
 
-    public function withoutParametrizedUrl($url)
+    protected function isSinglePropertyQuery($params)
     {
-        $url = explode('/', trim($url, '/'));
-        array_splice($url, -2);
-        return '/' . join('/', $url);
+        if (count($params) === 1 
+            && array_key_first($params) !== 'page' 
+            && is_countable($params[array_key_first($params)]) 
+            && count($params[array_key_first($params)]) === 1)
+            return true;
+        elseif (count($params) === 1
+            && !is_countable($params[array_key_first($params)]))
+            return true;
+    }
+
+    protected function isMultyPropertyQuery($params)
+    {
+        if (count($params) > 1)
+            return true;
+        elseif (count($params) && is_countable($params[array_key_first($params)]) && count($params[array_key_first($params)]) > 1)
+            return true;
     }
 }
