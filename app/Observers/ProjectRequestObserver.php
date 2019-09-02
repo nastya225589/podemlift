9d2022a8a86b7c63bb8715a4e5fd59ec9221f257
Kirill Osadhij <k.osadchij@molinos.ru>
2019-09-02 17:25:47 +0300

    Added email translations

--- app/Observers/ProjectRequestObserver.php
+++ app/Observers/ProjectRequestObserver.php
@@ -4,13 +4,15 @@ namespace App\Observers;
 
 use Notification;
 use App\Models\ProjectRequest;
+use App\Models\Settings;
 use App\Notifications\ProjectRequestNotify;
 
 class ProjectRequestObserver
 {
     public function created(ProjectRequest $projectRequest)
     {
-        Notification::route('mail', 'taylor@example.com')
+        $toMail = Settings::where('name', 'email')->first();
+        Notification::route('mail', $toMail->value)
                     ->notify(new ProjectRequestNotify($projectRequest));
     }
 }
