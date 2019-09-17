ffbfb853e3f0169ba8428b69be8c3d8e367c17a5
Kirill Osadchij <k.osadchij@molinos.ru>
2019-09-17 10:35:57 +0300

    Route regex fix

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::post('/request-form/send', 'RequestFormController@store');
 
 include 'catalog.php';
 
-Route::get('{url?}', 'PageController@show')->where('url', '[A-Za-z0-9/-]+');
+Route::get('{url?}', 'PageController@show')->where('url', '[A-Za-z0-9\/\-\%\w\[\]]+');
