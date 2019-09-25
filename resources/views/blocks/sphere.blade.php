a6978b189657592eca3c3461168b8729c91e8531
Анастасия Полякова <p.anastasia@molinos.ru>
2019-09-25 11:22:48 +0300

    HELPDESK-6402 add page sphere

---
+++ resources/views/blocks/sphere.blade.php
@@ -0,0 +1,36 @@
+<section class="sphere">
+    <div class="sphere__list">
+        <a href="/sphere-item" class="sphere__item">
+            <span class="sphere__number">01</span>
+            <span class="sphere__name">Банки</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">02</span>
+            <span class="sphere__name">Библиотеки</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">03</span>
+            <span class="sphere__name">Магазины</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">04</span>
+            <span class="sphere__name">Подъемники для зданий</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">05</span>
+            <span class="sphere__name">Подъемники для зоны разгрузки, отгрузки</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">06</span>
+            <span class="sphere__name">Рестораны и кафе</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">07</span>
+            <span class="sphere__name">Склады</span>
+        </a>
+        <a href="#" class="sphere__item">
+            <span class="sphere__number">08</span>
+            <span class="sphere__name">Музеи</span>
+        </a>
+    </div>
+</section>
