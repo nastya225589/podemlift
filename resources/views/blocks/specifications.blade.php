ebd8fbdb74e649e348bf92aab0ab74aac8828ba0
Kirill Osadchij <k.osadchij@molinos.ru>
2019-10-21 12:35:03 +0300

    Fix icon

--- resources/views/blocks/specifications.blade.php
+++ resources/views/blocks/specifications.blade.php
@@ -1,7 +1,7 @@
 <ul class="specifications__list">
     @foreach($product->properties as $prop)
         <li class="specifications__item">
-            <img class="specifications__img" src="{{ isset($prop->image) ? $prop->image->url : '/images/icon/weight-kg.svg' }}" alt="">
+            <img class="specifications__img" src="{{ isset($prop->image) ? $prop->image->url : '/images/icon/warranty.svg' }}" alt="">
             <span class="specifications__name">{{ $prop->name }}</span>
             <div class="specifications__dots">
                 <div class="specifications__dot"></div>
