6946a51b00edbbdabb5487c8a1afa211aebc76cb
Kirill Osadchij <k.osadchij@molinos.ru>
2019-10-25 11:50:14 +0300

    Added image block to TinyMce component

--- admin/resources/assets/js/components/Builder/Elements/TinyMce.js
+++ admin/resources/assets/js/components/Builder/Elements/TinyMce.js
@@ -13,15 +13,29 @@ export default class TinyMce extends Component {
 
   render () {
     return (
-      <Editor
-        initialValue={this.props.value}
-        init={{
-          plugins: 'link image code lists table',
-          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | numlist bullist | code',
-          table_toolbar: 'tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol'
-        }}
-        onChange={this.handleEditorChange}
-      />
+      <>
+        <Editor
+          initialValue={this.props.value}
+          init={{
+            plugins: 'link image code lists table',
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | numlist bullist | code',
+            table_toolbar: 'tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+            language: 'ru'
+          }}
+          onChange={this.handleEditorChange}
+        />
+        <div className="form-group">
+          <input type="file" className="upload-image-file" style={{display: 'none'}}/>
+          <div className="input-group" style={{justifyContent: 'flex-end'}}>
+              <div className="input-group-append upload-image-button">
+                  <span className="input-group-text" data-toggle="tooltip" title="Перетащите загруженное изображение в редактор">
+                      <span className="upload-image-preview"></span>
+                      <i className="fas fa-cloud-upload-alt"></i>
+                  </span>
+              </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
