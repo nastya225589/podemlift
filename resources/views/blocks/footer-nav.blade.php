fee3039529fc3a788aabed29b0c502525c4e0ff2
Анастасия Полякова <p.anastasia@molinos.ru>
2019-11-06 16:20:48 +0300

    HELPDESK-6682 footer

--- resources/views/blocks/footer-nav.blade.php
+++ resources/views/blocks/footer-nav.blade.php
@@ -1,44 +1,62 @@
 <nav class="footer-nav">
     <ul class="footer-nav__list footer-nav__list--main">
         <li class="footer-nav__item">
-            <a class="footer-nav__link" href="/">О компании</a>
+            <a class="footer-nav__link" href="/catalog">Kаталог</a>
         </li>
         <li class="footer-nav__item">
-            <a class="footer-nav__link" href="/">Производство</a>
+            <a class="footer-nav__link" href="/about">О компании</a>
         </li>
         <li class="footer-nav__item">
-            <a class="footer-nav__link" href="/">Объекты</a>
+            <a class="footer-nav__link" href="/production">Производство</a>
         </li>
         <li class="footer-nav__item">
-            <a class="footer-nav__link" href="/">Клиенты</a>
+            <a class="footer-nav__link" href="/objects">Наши объекты</a>
         </li>
         <li class="footer-nav__item">
-            <a class="footer-nav__link" href="/">Услуги</a>
+            <a class="footer-nav__link" href="/clients">Клиенты</a>
         </li>
-        <li class="footer-nav__item" id="footer-nav__toggle">
-            <a class="footer-nav__link footer-nav__link--submenu" href="/">Kаталог</a>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/services">Услуги</a>
         </li>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/poleznye-stranitsy">Полезные страницы</a>
+        </li>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/spheres-of-application">Сферы применения</a>
+        </li>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/poleznye-stranitsy/news">Новости</a>
+        </li>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/poleznye-stranitsy/faq">FAQ</a>
+        </li>
+        <li class="footer-nav__item">
+            <a class="footer-nav__link" href="/contacts">Контакты</a>
+        </li>
+        {{--<li class="footer-nav__item" id="footer-nav__toggle">--}}
+            {{--<a class="footer-nav__link footer-nav__link--submenu" href="/">Kаталог</a>--}}
+        {{--</li>--}}
     </ul>
     <div class="footer-nav__wrap">
         <ul class="footer-nav__list">
             <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/">Трособлочные подъемники</a>
+                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/trosoblochnye-podemniki">Трособлочные подъемники</a>
                 <div class="footer-submenu">
                     <ul class="footer-submenu__list">
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Kаталог</a>
+                            <a class="footer-nav__link" href="/catalog/trosoblochnye-podemniki/odnomachtovye-podemniki">Одномачтовые подъемники</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
+                            <a class="footer-nav__link" href="/catalog/trosoblochnye-podemniki/dvukhmachtovye-podemniki">Двухмачтовые подъемники</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Объекты</a>
+                            <a class="footer-nav__link" href="/catalog/trosoblochnye-podemniki/trekhmachtovye-podemniki">Трехмачтовые подъемники</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Клиенты</a>
+                            <a class="footer-nav__link" href="/catalog/trosoblochnye-podemniki/shakhtnye-podemniki">Шахтные подъемники</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link " href="/">Услуги</a>
+                            <a class="footer-nav__link " href="/catalog/trosoblochnye-podemniki/stoechnye-malye-podemniki">Стоечные (малые) подъемники</a>
                         </li>
                     </ul>
                 </div>
@@ -46,14 +64,20 @@
         </ul>
         <ul class="footer-nav__list">
             <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/">Гидравлические подъемники</a>
+                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/catalog/krany">Краны</a>
                 <div class="footer-submenu">
                     <ul class="footer-submenu__list">
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Kаталог</a>
+                            <a class="footer-nav__link" href="/catalog/krany/kozlovye-krany">Козловые краны</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
+                            <a class="footer-nav__link" href="/catalog/krany/konsolnye-krany">Консольные краны</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/krany/krany-pioner">Краны Пионер</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/krany/mostovye-krany">Мостовые краны</a>
                         </li>
                     </ul>
                 </div>
@@ -61,91 +85,80 @@
         </ul>
     </div>
     <div class="footer-nav__wrap">
+        <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/catalog/gidravlicheskie-podemniki-statsionarnye">Подъемники стационарные</a>
+                <div class="footer-submenu">
+                    <ul class="footer-submenu__list">
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-statsionarnye/nozhnichnye-podemniki">Ножничные подъемники</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-statsionarnye/gidravlicheskie-stoly">Гидравлические столы</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-statsionarnye/podemnye-stoly">Подъемные столы</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-statsionarnye/podemnye-platformy">Подъемные платформы</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/catalog/gidravlicheskie-podemniki-peredvizhnye">Подъемники передвижные</a>
+                <div class="footer-submenu">
+                    <ul class="footer-submenu__list">
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-statsionarnye/nozhnichnye-podemniki">Ножничные передвижные подъемники</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/gidravlicheskie-podemniki-peredvizhnye/peredvizhnye-gidravlicheskie-stoly">Передвижные гидравлические столы</a>
+                        </li>
+                        <li class="footer-nav__item">
+                            <a class="footer-nav__link" href="/catalog/naklonnye-gruzovye-podemniki">Наклонные грузовые подъемники</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="footer-nav__wrap">
+        <ul class="footer-nav__list">
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold" href="/catalog/vzryvozashchishchennoe-oborudovanie">Взрывозащищенное оборудование</a>
+            </li>
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold" href="/catalog/unikalnoe-gruzopodemnoe-oborudovanie">Уникальное грузоподъемное оборудование</a>
+            </li>
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold" href="/catalog/prochee-oborudovanie">Прочее оборудование</a>
+            </li>
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold" href="/catalog/sfery-primeneniya">Сферы применения</a>
+            </li>
+            <li class="footer-nav__item">
+                <a class="footer-nav__link footer-nav__link--bold" href="/catalog/podemnaya-platforma-s-vertikalnym-peremeshcheniem-1">Подъемная платформа с вертикальным перемещением</a>
+            </li>
+        </ul>
         <ul class="footer-nav__list">
             <li class="footer-nav__item">
                 <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/">Лифты</a>
                 <div class="footer-submenu">
                     <ul class="footer-submenu__list">
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Kаталог</a>
+                            <a class="footer-nav__link" href="/catalog/tekhnicheskie-lifty">Технические лифты</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
+                            <a class="footer-nav__link" href="/catalog/gruzovye-lifty">Грузовые лифты</a>
                         </li>
                         <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
+                            <a class="footer-nav__link" href="/catalog/malye-gruzovye-lifty">Малые грузовые лифты</a>
                         </li>
                     </ul>
                 </div>
-            </li>
-        </ul>
-        <ul class="footer-nav__list">
-            <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/">Краны</a>
-                <div class="footer-submenu">
-                    <ul class="footer-submenu__list">
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Kаталог</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="footer-nav__wrap">
-        <ul class="footer-nav__list">
-            <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold footer-nav__link--submenu" href="/">Прочее оборудование</a>
-                <div class="footer-submenu">
-                    <ul class="footer-submenu__list">
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Kаталог</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                        <li class="footer-nav__item">
-                            <a class="footer-nav__link" href="/">Производство</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="footer-nav__wrap">
-        <ul class="footer-nav__list">
-            <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold" href="/">Наклонные грузовые подъемники</a>
-            </li>
-            <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold" href="/">Уникальное грузоподъемное оборудование</a>
-            </li>
-            <li class="footer-nav__item">
-                <a class="footer-nav__link footer-nav__link--bold" href="/">Взрывозащищенное оборудование</a>
             </li>
         </ul>
     </div>
