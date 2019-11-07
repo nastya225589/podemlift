c84e7da58329251e94840644b6a9ac629bb3f5e2
Kirill Osadchij <k.osadchij@molinos.ru>
2019-11-07 11:53:22 +0300

    Added services bg

--- resources/views/blocks/services-page.blade.php
+++ resources/views/blocks/services-page.blade.php
@@ -1,8 +1,30 @@
-@php ($serviceCategories = \App\Models\ServiceCategory::all())
+@php
+    $serviceCategories = \App\Models\ServiceCategory::all();
+@endphp
 
 <section class="services-page">
     @foreach ($serviceCategories as $category)
-        <div class="services-page__wrap" style="background-image: url(/images/bg/service_bg_1.jpg);">
+        @php
+            switch ($category->name) {
+                case 'Разработка и монтаж':
+                    $background = '/images/bg/service_bg_1.jpg';
+                    break;
+                case 'Обслуживание и модернизация':
+                    $background = '/images/bg/service_bg_2.jpg';
+                    break;
+                case 'Ремонт и реставрация':
+                    $background = '/images/bg/service_bg_3.jpg';
+                    break;
+                case 'Консультация и экспертиза':
+                    $background = '/images/bg/service_bg_4.jpg';
+                    break;
+                
+                default:
+                    $background = '/images/bg/service_bg_1.jpg';
+                    break;
+            }  
+        @endphp
+        <div class="services-page__wrap" style="background-image: url({{ $background }});">
             <div class="services-page__title title-h2">{{ $category->name }}</div>
             <div class="services-page__inner">
                 @foreach ($category->services as $item)
