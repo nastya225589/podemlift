afb77d88d6508cf02f1b80ad4577698a0af8d655
Анастасия Полякова <p.anastasia@molinos.ru>
2019-11-07 12:19:23 +0300

    HELPDESK-6682 social

--- resources/views/blocks/social.blade.php
+++ resources/views/blocks/social.blade.php
@@ -1,42 +1,42 @@
 <div class="social">
     <ul class="social__list">
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="https://vk.com/podyemnoye_oborudovaniye" class="social__link" target="_blank">
                 <svg width="30" height="29">
                     <use xlink:href="images/icon/sprite.svg#vk"></use>
                 </svg>
             </a>
         </li>
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="https://www.facebook.com/%D0%9F%D0%BE%D0%B4%D1%8A%D0%B5%D0%BC%D0%BD%D0%BE%D0%B5-%D0%BE%D0%B1%D0%BE%D1%80%D1%83%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-203985509808070/" class="social__link" target="_blank">
                 <svg width="30" height="29">
                     <use xlink:href="images/icon/sprite.svg#facebook"></use>
                 </svg>
             </a>
         </li>
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="https://www.instagram.com/p_ob.ru/?hl=ru" class="social__link" target="_blank">
                 <svg width="40" height="39">
                     <use xlink:href="images/icon/sprite.svg#instagram"></use>
                 </svg>
             </a>
         </li>
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="https://api.whatsapp.com/send?phone=79052228678" class="social__link" target="_blank">
                 <svg width="30" height="29">
                     <use xlink:href="images/icon/sprite.svg#exclude"></use>
                 </svg>
             </a>
         </li>
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="/" class="social__link" target="_blank">
                 <svg width="40" height="39">
                     <use xlink:href="images/icon/sprite.svg#viber"></use>
                 </svg>
             </a>
         </li>
         <li class="social__item">
-            <a href="/" class="social__link">
+            <a href="https://t.me/Podemnoe" class="social__link" target="_blank">
                 <svg width="30" height="29">
                     <use xlink:href="images/icon/sprite.svg#telegram"></use>
                 </svg>
