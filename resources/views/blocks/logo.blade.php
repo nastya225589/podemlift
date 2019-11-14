7554656cf98d4ffacf58e4a3433e799040a6c057
Анастасия Полякова <p.anastasia@molinos.ru>
2019-11-14 11:07:28 +0300

    HELPDESK-6717 header new logo

--- resources/views/blocks/logo.blade.php
+++ resources/views/blocks/logo.blade.php
@@ -1,6 +1,6 @@
 <a class="logo" href="/">
     <svg width="398" height="85">
-        <use xlink:href="/images/icon/sprite.svg#logo"></use>
+        <use xlink:href="/images/icon/sprite.svg#logo-pob"></use>
     </svg>
     {{--<img src="/images/logo/logo.svg"  alt='Подъемное оборудование'>--}}
     {{--<strong>Подъемное оборудование</strong>--}}
